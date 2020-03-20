29c023fa02f28df999dd017542092151796087cb
djy <137359049@qq.com>
2020-03-20 21:16:37 +0800

    1.对于图片选择进行适配

--- fly/src/main/java/com/tiamosu/fly/utils/FileUtils.kt
+++ fly/src/main/java/com/tiamosu/fly/utils/FileUtils.kt
@@ -2,8 +2,13 @@
 
 package com.tiamosu.fly.utils
 
+import android.net.Uri
+import com.blankj.utilcode.util.CloseUtils
 import com.blankj.utilcode.util.Utils
+import java.io.BufferedOutputStream
 import java.io.File
+import java.io.FileOutputStream
+import java.io.InputStream
 
 /**
  * @author tiamosu
@@ -33,6 +38,7 @@ fun createOrExistsDir(file: File?): File? {
 
 /**
  * 返回缓存文件夹
+ * 应用在卸载后，会将App-specific目录下的数据删除，如果在AndroidManifest.xml中声明：android:hasFragileUserData="true"用户可以选择是否保留。
  */
 fun getCacheFile(): File {
     //获取应用程序内的外部缓存路劲
@@ -41,4 +47,39 @@ fun getCacheFile(): File {
     cacheFile = cacheFile ?: createDir(Utils.getApp().packageName)
     //如果获取的文件为空，就使用应用程序内的内部缓存路劲
     return cacheFile ?: Utils.getApp().cacheDir
+}
+
+/**
+ * 对于图片选择进行适配（对于媒体资源的访问，返回Uri后，再转为File）
+ */
+fun uriToFile(uri: Uri): File? {
+    val imgFile = createOrExistsDir(Utils.getApp().getExternalFilesDir("image"))
+    val file = File(imgFile?.absolutePath + File.separator + System.currentTimeMillis() + ".jpg")
+    var bis: InputStream? = null
+    var fos: FileOutputStream? = null
+    var bos: BufferedOutputStream? = null
+
+    try {
+        // 使用openInputStream(uri)方法获取字节输入流
+        bis = Utils.getApp().contentResolver.openInputStream(uri)
+        fos = FileOutputStream(file)
+        bos = BufferedOutputStream(fos)
+
+        val bytes = ByteArray(1024)
+        var read: Int
+        do {
+            read = bis?.read(bytes) ?: -1
+            if (read == -1) break
+
+            bos.write(bytes, 0, read)
+        } while (true)
+
+        bos.flush()
+        fos.flush()
+    } catch (e: Exception) {
+        return null
+    } finally {
+        CloseUtils.closeIO(bos, fos, bis)
+    }
+    return file
 }
