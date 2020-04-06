86e6a0a14f77077f7aafdcdfd9a986197144ad9f
djy <137359049@qq.com>
2020-04-06 16:37:52 +0800

    1.utilcode 1.27.7 → 1.27.2

--- buildSrc/src/main/kotlin/Configuration.kt
+++ buildSrc/src/main/kotlin/Configuration.kt
@@ -14,7 +14,7 @@ object Versions {
     const val lifecycle = "2.2.0"
     const val kotlin = "1.3.71"
     const val retrofit2 = "2.8.1"
-    const val okhttp3 = "4.4.1" //版本3.12.X，兼容Android 4
+    const val okhttp3 = "4.4.1"
     const val glide = "4.11.0"
     const val dagger2 = "2.27"
     const val fragmentation = "1.1.8"
@@ -78,7 +78,7 @@ object Deps {
     const val rxerrorhandler = "me.jessyan:rxerrorhandler:2.1.1"
 
     //utilcode
-    const val utilcode = "com.blankj:utilcodex:1.27.1"
+    const val utilcode = "com.blankj:utilcodex:1.27.2"
 
     //disklrucache
     const val disklrucache = "com.jakewharton:disklrucache:2.0.2"
