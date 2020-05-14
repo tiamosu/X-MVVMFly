71dd3d05299eb34d933a10a55d0e9f155c4edc38
djy <137359049@qq.com>
2020-05-15 00:57:03 +0800

    1.版本更新：1.1.3

--- buildSrc/src/main/kotlin/Configuration.kt
+++ buildSrc/src/main/kotlin/Configuration.kt
@@ -25,7 +25,7 @@ object Versions {
 object Publish {
     const val userOrg = "weixia" //bintray.com用户名
     const val groupId = "me.tiamosu" //jcenter上的路径
-    const val publishVersion = "1.1.2" //版本号
+    const val publishVersion = "1.1.3" //版本号
     const val desc = "Oh hi, this is a nice description for a project, right?"
     const val website = "https://github.com/tiamosu/X-MVVMFly"
     const val gitUrl = "https://github.com/tiamosu/X-MVVMFly.git"
