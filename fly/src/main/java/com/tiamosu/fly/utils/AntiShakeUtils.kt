053c15a40d255e25282b60220bcc78dda46efbb9
djy <137359049@qq.com>
2020-05-29 12:42:35 +0800

    1.AS 升级 4.0 2.点击防抖优化

--- fly/src/main/java/com/tiamosu/fly/utils/AntiShakeUtils.kt
+++ fly/src/main/java/com/tiamosu/fly/utils/AntiShakeUtils.kt
@@ -11,18 +11,22 @@ import android.view.View
  * @date 2020/3/18.
  */
 
-private const val DEFAULT_DURATION = 200L
 private const val TAG_KEY = 0x7EFFFFFF
 
 @JvmOverloads
-fun isValid(view: View, duration: Long = DEFAULT_DURATION): Boolean {
+fun isValid(view: View, duration: Long = 200L): Boolean {
     val curTime = System.currentTimeMillis()
-    val tag = view.getTag(TAG_KEY)
-    if (tag !is Long) {
+    val preTime = view.getTag(TAG_KEY)
+    if (preTime !is Long) {
         view.setTag(TAG_KEY, curTime)
         return true
     }
-    if (curTime - tag <= duration) return false
+    if (curTime - preTime < 0) {
+        view.setTag(TAG_KEY, curTime)
+        return false
+    } else if (curTime - preTime <= duration) {
+        return false
+    }
     view.setTag(TAG_KEY, curTime)
     return true
 }
