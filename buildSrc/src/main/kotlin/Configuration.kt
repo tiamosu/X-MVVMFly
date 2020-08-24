be13055514da018e0b6cf5984d808024c6c231e7
djy <137359049@qq.com>
2020-08-24 09:24:27 +0800

    1.调试

--- buildSrc/src/main/kotlin/Configuration.kt
+++ buildSrc/src/main/kotlin/Configuration.kt
@@ -37,15 +37,15 @@ object Deps {
     const val androidx_constraint_layout = "androidx.constraintlayout:constraintlayout:1.1.3"
     const val androidx_core_ktx = "androidx.core:core-ktx:1.3.1"
     const val androidx_viewpager2 = "androidx.viewpager2:viewpager2:1.0.0"
-    const val androidx_fragment_ktx = "androidx.fragment:fragment-ktx:1.2.5"
 
     //navigation
+    const val androidx_fragment_ktx = "androidx.fragment:fragment-ktx:1.2.5"
+    const val androidx_navigation_runtime =
+        "androidx.navigation:navigation-runtime:${Versions.navigation}"
     const val androidx_navigation_fragment_ktx =
         "androidx.navigation:navigation-fragment-ktx:${Versions.navigation}"
     const val androidx_navigation_ui_ktx =
         "androidx.navigation:navigation-ui-ktx:${Versions.navigation}"
-    const val androidx_navigation_runtime =
-        "androidx.navigation:navigation-runtime:${Versions.navigation}"
 
     //lifecycle
     const val lifecycle_runtime = "androidx.lifecycle:lifecycle-runtime:${Versions.lifecycle}"
