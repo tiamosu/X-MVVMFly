85a45ca1739f912a24472d3d753685d7b8558a0f
djy <137359049@qq.com>
2020-08-25 14:45:49 +0800

    1.Glide加载图片调试

--- app/src/main/java/com/tiamosu/fly/demo/ui/fragments/GlideFragment.kt
+++ app/src/main/java/com/tiamosu/fly/demo/ui/fragments/GlideFragment.kt
@@ -5,8 +5,8 @@ import com.blankj.utilcode.util.ToastUtils
 import com.bumptech.glide.request.target.DrawableImageViewTarget
 import com.bumptech.glide.request.transition.Transition
 import com.tiamosu.fly.core.base.BaseFragment
-import com.tiamosu.fly.ext.clickNoRepeat
 import com.tiamosu.fly.demo.R
+import com.tiamosu.fly.ext.clickNoRepeat
 import com.tiamosu.fly.http.imageloader.ImageLoader
 import com.tiamosu.fly.imageloader.glide.ImageConfigImpl
 import com.tiamosu.fly.imageloader.glide.TranscodeType
@@ -18,6 +18,13 @@ import kotlinx.android.synthetic.main.fragment_glide.*
  */
 class GlideFragment : BaseFragment() {
 
+    companion object {
+        const val IMG_URL = "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000" +
+                "&sec=1584613638363&di=f3145501f1c82ecce1fad4937d0b6fc2&imgtype=0" +
+                "&src=http%3A%2F%2Fpics4.baidu.com%2Ffeed%2F2cf5e0fe9925bc31bfe6323c3f89ddb7ca1370b3.jpeg" +
+                "%3Ftoken%3D8c0dbbd8c71510e420916599992f67bc%26s%3D4BA438626AD163E94D1421DE0000C0E2"
+    }
+
     override fun getLayoutId() = R.layout.fragment_glide
 
     override fun initEvent() {
@@ -27,20 +34,18 @@ class GlideFragment : BaseFragment() {
                     .load(R.drawable.fly)
                     .crossFade()
                     .centerInside()
+                    .override(150, 150)
                     .into(iv)
                     .build()
             )
         }
 
         btn_load_net_pic.clickNoRepeat {
-            val imgUrl = "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000" +
-                    "&sec=1584613638363&di=f3145501f1c82ecce1fad4937d0b6fc2&imgtype=0" +
-                    "&src=http%3A%2F%2Fpics4.baidu.com%2Ffeed%2F2cf5e0fe9925bc31bfe6323c3f89ddb7ca1370b3.jpeg" +
-                    "%3Ftoken%3D8c0dbbd8c71510e420916599992f67bc%26s%3D4BA438626AD163E94D1421DE0000C0E2"
             ImageLoader.loadImage(
                 ImageConfigImpl
-                    .load(imgUrl)
+                    .load(IMG_URL)
                     .`as`(TranscodeType.AS_DRAWABLE)
+                    .override(300, 300)
                     .into(object : DrawableImageViewTarget(iv) {
                         override fun onResourceReady(
                             resource: Drawable,
