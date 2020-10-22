54f647c346d033da3de6bf6e5bdeb0e9a2b2a78b
djy <137359049@qq.com>
2020-10-22 14:14:16 +0800

    1.ViewPager2 嵌套 ViewPager2或者 嵌套recyclerview，冲突问题优化

--- app/src/main/java/com/tiamosu/fly/demo/ui/views/NestedScrollableHost.kt
+++ app/src/main/java/com/tiamosu/fly/demo/ui/views/NestedScrollableHost.kt
@@ -5,10 +5,10 @@ import android.util.AttributeSet
 import android.view.MotionEvent
 import android.view.View
 import android.view.ViewConfiguration
-import android.view.ViewParent
 import android.widget.FrameLayout
 import androidx.viewpager2.widget.ViewPager2
-import kotlin.math.abs
+import androidx.viewpager2.widget.ViewPager2.ORIENTATION_HORIZONTAL
+import kotlin.math.absoluteValue
 import kotlin.math.sign
 
 /**
@@ -18,58 +18,34 @@ import kotlin.math.sign
  * @date 2020/10/16.
  */
 class NestedScrollableHost : FrameLayout {
+    constructor(context: Context) : super(context)
+    constructor(context: Context, attrs: AttributeSet?) : super(context, attrs)
+
     private var touchSlop = 0
     private var initialX = 0f
     private var initialY = 0f
-    private var findViewCacheMap: HashMap<Int, View>? = null
-
-    constructor(context: Context) : this(context, null)
-
-    constructor(context: Context, attrs: AttributeSet?) : super(context, attrs) {
-        val configuration = ViewConfiguration.get(context)
-        this.touchSlop = configuration.scaledTouchSlop
-    }
-
-    private fun getParentViewPager(): ViewPager2? {
-        var viewParent: ViewParent? = this.parent
-        if (viewParent !is View) {
-            viewParent = null
-        }
-        var v = viewParent as? View
-        while (v != null && v !is ViewPager2) {
-            viewParent = v.parent
-            if (viewParent !is View) {
-                viewParent = null
+    private val parentViewPager: ViewPager2?
+        get() {
+            var v: View? = parent as? View
+            while (v != null && v !is ViewPager2) {
+                v = v.parent as? View
             }
-            v = viewParent as? View
+            return v as? ViewPager2
         }
-        var v2 = v
-        if (v !is ViewPager2) {
-            v2 = null
-        }
-        return v2 as? ViewPager2
-    }
 
-    private fun getChild(): View? {
-        return if (this.childCount > 0) getChildAt(0) else null
+    private val child: View? get() = if (childCount > 0) getChildAt(0) else null
+
+    init {
+        touchSlop = ViewConfiguration.get(context).scaledTouchSlop
     }
 
     private fun canChildScroll(orientation: Int, delta: Float): Boolean {
-        val direction = -sign(delta).toInt()
-        val view: View?
-        var isCanChildScroll = false
-        when (orientation) {
-            0 -> {
-                view = getChild()
-                isCanChildScroll =
-                    view?.canScrollHorizontally(direction) ?: false
-            }
-            1 -> {
-                view = getChild()
-                isCanChildScroll = view?.canScrollVertically(direction) ?: false
-            }
+        val direction = -delta.sign.toInt()
+        return when (orientation) {
+            0 -> child?.canScrollHorizontally(direction) ?: false
+            1 -> child?.canScrollVertically(direction) ?: false
+            else -> throw IllegalArgumentException()
         }
-        return isCanChildScroll
     }
 
     override fun onInterceptTouchEvent(e: MotionEvent): Boolean {
@@ -78,51 +54,41 @@ class NestedScrollableHost : FrameLayout {
     }
 
     private fun handleInterceptTouchEvent(e: MotionEvent) {
-        val viewPager2 = getParentViewPager()
-        if (viewPager2 != null) {
-            val orientation = viewPager2.orientation
-            if (canChildScroll(orientation, -1.0f) || canChildScroll(orientation, 1.0f)) {
-                if (e.action == 0) {
-                    initialX = e.x
-                    initialY = e.y
-                    this.parent.requestDisallowInterceptTouchEvent(true)
-                } else if (e.action == 2) {
-                    val dx = e.x - initialX
-                    val dy = e.y - initialY
-                    val isVpHorizontal = orientation == 0
-                    val scaledDx = abs(dx) * if (isVpHorizontal) 0.5f else 1.0f
-                    val scaledDy = abs(dy) * if (isVpHorizontal) 1.0f else 0.5f
-                    if (scaledDx > touchSlop.toFloat() || scaledDy > touchSlop.toFloat()) {
-                        when {
-                            isVpHorizontal == scaledDy > scaledDx -> {
-                                this.parent.requestDisallowInterceptTouchEvent(false)
-                            }
-                            canChildScroll(orientation, if (isVpHorizontal) dx else dy) -> {
-                                this.parent.requestDisallowInterceptTouchEvent(true)
-                            }
-                            else -> {
-                                this.parent.requestDisallowInterceptTouchEvent(false)
-                            }
-                        }
+        val orientation = parentViewPager?.orientation ?: return
+
+        // Early return if child can't scroll in same direction as parent
+        if (!canChildScroll(orientation, -1f) && !canChildScroll(orientation, 1f)) {
+            return
+        }
+
+        if (e.action == MotionEvent.ACTION_DOWN) {
+            initialX = e.x
+            initialY = e.y
+            parent.requestDisallowInterceptTouchEvent(true)
+        } else if (e.action == MotionEvent.ACTION_MOVE) {
+            val dx = e.x - initialX
+            val dy = e.y - initialY
+            val isVpHorizontal = orientation == ORIENTATION_HORIZONTAL
+
+            // assuming ViewPager2 touch-slop is 2x touch-slop of child
+            val scaledDx = dx.absoluteValue * if (isVpHorizontal) 0.5f else 1f
+            val scaledDy = dy.absoluteValue * if (isVpHorizontal) 1f else 0.5f
+
+            if (scaledDx > touchSlop || scaledDy > touchSlop) {
+                if (isVpHorizontal == (scaledDy > scaledDx)) {
+                    // Gesture is perpendicular, allow all parents to intercept
+                    parent.requestDisallowInterceptTouchEvent(false)
+                } else {
+                    // Gesture is parallel, query child if movement in that direction is possible
+                    if (canChildScroll(orientation, if (isVpHorizontal) dx else dy)) {
+                        // Child can scroll, disallow all parents to intercept
+                        parent.requestDisallowInterceptTouchEvent(true)
+                    } else {
+                        // Child cannot scroll, allow all parents to intercept
+                        parent.requestDisallowInterceptTouchEvent(false)
                     }
                 }
             }
         }
-    }
-
-    fun findCachedViewById(var1: Int): View? {
-        if (findViewCacheMap == null) {
-            findViewCacheMap = HashMap()
-        }
-        var var2: View? = findViewCacheMap?.get(var1)
-        if (var2 == null) {
-            var2 = findViewById(var1)
-            findViewCacheMap?.put(var1, var2)
-        }
-        return var2
-    }
-
-    fun clearFindViewByIdCache() {
-        findViewCacheMap?.clear()
     }
 }
