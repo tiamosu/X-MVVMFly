d1680d078746175fdc7132a7f2fcaffc92aa1cfc
djy <137359049@qq.com>
2020-12-04 10:22:49 +0800

    1.LiveData addObserve 优化

--- fly/src/main/java/com/tiamosu/fly/ext/LiveDataExt.kt
+++ fly/src/main/java/com/tiamosu/fly/ext/LiveDataExt.kt
@@ -3,12 +3,14 @@ package com.tiamosu.fly.ext
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.LiveData
+import com.tiamosu.fly.callback.EventLiveData
 
 /**
  * 添加 LiveData 观察者
  */
 fun <T> Fragment.addObserve(liveData: LiveData<T>?, onChanged: (T?) -> Unit) {
-    liveData?.observe(this) { t: T? ->
+    val owner = if (liveData is EventLiveData) this else viewLifecycleOwner
+    liveData?.observe(owner) { t: T? ->
         onChanged.invoke(t)
     }
 }
