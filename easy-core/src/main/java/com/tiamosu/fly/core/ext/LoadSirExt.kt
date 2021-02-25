11c72fff62f3d73c94f1af688d7e0656052b4d20
djy <137359049@qq.com>
2021-02-25 16:41:26 +0800

    1.loadSir 调试

--- easy-core/src/main/java/com/tiamosu/fly/core/ext/LoadSirExt.kt
+++ easy-core/src/main/java/com/tiamosu/fly/core/ext/LoadSirExt.kt
@@ -1,15 +1,19 @@
 package com.tiamosu.fly.core.ext
 
+import android.content.Context
 import android.view.View
+import androidx.appcompat.widget.AppCompatTextView
 import com.kingja.loadsir.callback.Callback
 import com.kingja.loadsir.callback.SuccessCallback
 import com.kingja.loadsir.core.LoadService
 import com.kingja.loadsir.core.LoadSir
+import com.tiamosu.fly.core.R
 import com.tiamosu.fly.core.base.BaseFragment
 import com.tiamosu.fly.core.ui.loadsir.CustomCallback
 import com.tiamosu.fly.core.ui.loadsir.EmptyCallback
 import com.tiamosu.fly.core.ui.loadsir.ErrorCallback
 import com.tiamosu.fly.core.ui.loadsir.LoadingCallback
+import com.tiamosu.fly.ext.clickNoRepeat
 
 /**
  * @author tiamosu
@@ -34,6 +38,30 @@ fun BaseFragment.loadServiceInit(
         }
 }
 
+/**
+ * loadService 显示 callback
+ */
 inline fun <reified T : Callback> LoadService<*>.showCallback() {
-    return showCallback(T::class.java)
+    setErrorRetry<T>()
+    showCallback(T::class.java)
+}
+
+inline fun <reified T : Callback> LoadService<*>.setCallback(
+    crossinline transport: (context: Context, view: View?, callback: Callback) -> Unit
+) {
+    setCallBack(T::class.java) { context, view, callback ->
+        transport.invoke(context, view, callback)
+    }
+}
+
+inline fun <reified T : Callback> LoadService<*>.setErrorRetry() {
+    setCallback<T> { _, view, callback ->
+        when (callback::class) {
+            ErrorCallback::class -> {
+                view?.findViewById<AppCompatTextView>(R.id.loadError_tvRetry)?.clickNoRepeat {
+                    callback.obtainReloadListener()?.onReload(view)
+                }
+            }
+        }
+    }
 }
