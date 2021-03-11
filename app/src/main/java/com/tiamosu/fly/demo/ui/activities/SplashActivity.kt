351b819a52475f96adc4704798af80f8984ca769
djy <137359049@qq.com>
2021-03-11 16:44:02 +0800

    1.开屏页调试

--- app/src/main/java/com/tiamosu/fly/demo/ui/activities/SplashActivity.kt
+++ app/src/main/java/com/tiamosu/fly/demo/ui/activities/SplashActivity.kt
@@ -1,5 +1,7 @@
 package com.tiamosu.fly.demo.ui.activities
 
+import android.content.Intent
+import android.os.Bundle
 import android.util.Log
 import com.blankj.utilcode.util.ActivityUtils
 import com.tiamosu.fly.base.DataBindingConfig
@@ -17,6 +19,24 @@ class SplashActivity : BaseActivity() {
         addBindingParam(BR.click, ClickProxy())
     }
 
+    override fun onCreateInit(savedInstanceState: Bundle?): Boolean {
+        if (!super.onCreateInit(savedInstanceState)) {
+            return false
+        }
+        //为了防止重复启动多个闪屏页面
+        if (!isTaskRoot) {
+            val intent = intent
+            if (intent != null
+                && intent.hasCategory(Intent.CATEGORY_LAUNCHER)
+                && Intent.ACTION_MAIN == intent.action
+            ) {
+                finish()
+                return false
+            }
+        }
+        return true
+    }
+
     inner class ClickProxy {
 
         fun jumpToMain() {
@@ -32,5 +52,16 @@ class SplashActivity : BaseActivity() {
 
     override fun onNetworkStateChanged(isConnected: Boolean) {
         Log.e("tiamosu", "onNetworkStateChanged:$isConnected")
+    }
+
+    @Suppress("RedundantOverride")
+    override fun onDestroy() {
+        //因为修复了一个启动页被重复启动的问题，所以有可能 Activity 还没有初始化完成就已经销毁了
+        //所以如果需要在此处释放对象资源需要先对这个对象进行判空，否则可能会导致空指针异常
+        super.onDestroy()
+    }
+
+    //拦截返回退出应用
+    override fun onBackPressedSupport() {
     }
 }
