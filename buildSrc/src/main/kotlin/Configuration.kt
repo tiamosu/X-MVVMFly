e815aab5807bf4fa03a0709b40d80c3eff4cf6f9
djy <137359049@qq.com>
2021-03-19 19:53:07 +0800

    1.update publish

--- buildSrc/src/main/kotlin/Configuration.kt
+++ buildSrc/src/main/kotlin/Configuration.kt
@@ -43,13 +43,7 @@ object Versions {
 }
 
 object Publish {
-    const val userOrg = "weixia" //bintray.com用户名
-    const val groupId = "me.tiamosu" //jcenter上的路径
-    const val publishVersion = "2.1.7" //版本号
-    const val desc = "Oh hi, this is a nice description for a project, right?"
-    const val website = "https://github.com/tiamosu/X-MVVMFly"
-    const val gitUrl = "https://github.com/tiamosu/X-MVVMFly.git"
-    const val email = "djy2009wenbi@gmail.com"
+    const val groupId = "com.github.tiamosu"
 }
 
 object Deps {
