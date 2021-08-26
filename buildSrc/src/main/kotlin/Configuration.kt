2c37a1676907f303729a20c8940f6d586eaadf69
djy <137359049@qq.com>
2021-08-26 15:01:41 +0800

    1.rxjava = "3.1.0"

--- buildSrc/src/main/kotlin/Configuration.kt
+++ buildSrc/src/main/kotlin/Configuration.kt
@@ -20,7 +20,7 @@ object Versions {
     const val constraintlayout = "2.1.0"
     const val recyclerview = "1.2.1"
 
-    const val rxjava = "3.0.13"
+    const val rxjava = "3.1.0"
     const val rxandroid = "3.0.0"
     const val retrofit2 = "2.9.0"
     const val okhttp3 = "4.9.1"
