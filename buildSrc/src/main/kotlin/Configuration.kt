c5d5a06cabeba4fd7fe05c6daa604efa5f13f760
djy <137359049@qq.com>
2022-03-17 22:50:07 +0800

    1.fly_navigation = "1.6.3" 2.compileSdk = 31

--- buildSrc/src/main/kotlin/Configuration.kt
+++ buildSrc/src/main/kotlin/Configuration.kt
@@ -1,7 +1,7 @@
 @file:Suppress("unused", "SpellCheckingInspection")
 
 object Android {
-    const val compileSdk = 32
+    const val compileSdk = 31
     const val minSdk = 21
     const val targetSdk = 30
 
@@ -29,7 +29,7 @@ object Versions {
     const val leakcanary = "2.8.1"
     const val unpeeklivedata = "7.2.0-beta1"
 
-    const val fly_navigation = "1.6.2"
+    const val fly_navigation = "1.6.3"
     const val rxerrorhandler = "4.0.1"
     const val loadsir = "3.0.0"
 }
